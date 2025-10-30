103e07bb9474d6f305da93eb33443973714e3d9c
DavidYordan <xinburuquan@gmail.com>
2025-10-30 08:45:10 +0800

    update

--- QuantService/test.py
+++ QuantService/test.py
@@ -10,19 +10,26 @@ PARAMS = {
     "interval": "1m",
     "startTime": 3840060000,
     "endTime": 3900059999,
-    "limit": 1000,
+    "limit": 10,
 }
 
+# async def main():
+#     async with httpx.AsyncClient(proxies=PROXY, timeout=10) as client:
+#         try:
+#             resp = await client.get(URL, params=PARAMS)
+#             print(f"状态码: {resp.status_code}")
+#             print("响应内容:")
+#         except Exception as e:
+#             print("请求出错:", e)
 async def main():
-    async with httpx.AsyncClient(proxies=PROXY, timeout=10) as client:
+    transport = httpx.AsyncHTTPTransport(proxies=PROXY)
+    async with httpx.AsyncClient(transport=transport, timeout=10) as client:
         try:
             resp = await client.get(URL, params=PARAMS)
             print(f"状态码: {resp.status_code}")
             print("响应内容:")
-            print(resp.text[:500])  # 打印前500个字符以防太长
         except Exception as e:
             print("请求出错:", e)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
